657d900637d45828adf1b24292fafc3ac17d6f83
Matthew Harris <matthewharrisak@gmail.com>
2021-03-14 18:03:02 -0500

    changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import './App.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          this is your hair cut info form
+          this is your hair cut info form // I am writing things to test the deployment
         </p>
         
       </header>
